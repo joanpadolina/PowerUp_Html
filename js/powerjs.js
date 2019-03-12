432fba00ca35a7f506f99049b9fae40ca74c60f8
Joan <Joanpadolina@gmail.com>
2019-03-12 12:35:27 +0100

    Background-fix Menubar slide icons added ( who and pillars) Icons introductie CO founders

---
+++ js/powerjs.js
@@ -0,0 +1,14 @@
+console.log('hi');
+
+
+var navSide = document.querySelector('.navcontain');
+var menuKnop = document.querySelector('.menu');
+var sluitNav = document.querySelector('.sluitmenu');
+
+menuKnop.addEventListener('click', function(){
+  navSide.classList.toggle('show-nav');
+});
+
+sluitNav.addEventListener('click', function(){
+  navSide.classList.remove('show-nav');
+});
